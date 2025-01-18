423e71ca789138a320f9e03890d391a7091637a6
Dviih <dviih2s@gmail.com>
2025-01-18 17:06:45 -0300

    Wait: A simpler sync.WaitGroup.

---
+++ wait.go
@@ -0,0 +1,36 @@
+/*
+ *     Drop-in replacement for Go's sync featuring generics and channels.
+ *     Copyright (C) 2025  Dviih
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package sync
+
+type Wait struct {
+	c chan bool
+}
+
+func (wait *Wait) Done() {
+	wait.c <- true
+}
+
+func (wait *Wait) Wait() {
+	if wait.c == nil {
+		wait.c = make(chan bool)
+	}
+
+	<-wait.c
+}
